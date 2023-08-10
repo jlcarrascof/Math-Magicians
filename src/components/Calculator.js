cd8712446e99f3c59e7981684a3ff8f3a3fe3ffe
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-08-10 17:52:36 -0400

    Add new components with styles

---
+++ src/components/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+import Buttons from './Buttons';
+
+function Calculator() {
+  return (
+    <div className="calc">
+      <Buttons />
+    </div>
+  );
+}
+export default Calculator;
