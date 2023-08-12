cfb8b4a4927d73ff40e8a295f4190de1abc4ff48
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-08-11 23:40:09 -0400

    Update Calculator.js component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../App.css';
 import Buttons from './Buttons';
+import QuoteContainer from './QuoteContainer';
 
 function Calculator() {
   return (
-    <div className="calc">
-      <Buttons />
-    </div>
+    <body>
+      <h1 className="title">Math Magicians</h1>
+      <div className="calc">
+        <Buttons />
+        <QuoteContainer />
+      </div>
+    </body>
   );
 }
+
 export default Calculator;
