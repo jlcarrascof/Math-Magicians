f8666f273894492b0e0bc2d574cf5bd16a7ef49a
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-08-16 23:05:55 -0400

    Update index.js to include React Router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './App.css';
+import './styles/App.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
