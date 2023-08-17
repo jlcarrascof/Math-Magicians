cca68e3cb1225a32adfe551d425f622fbc06aa70
Javier Martinez <entrenamientospuntonet@gmail.com>
2023-08-16 23:02:46 -0400

    Create Quote.js page

---
+++ src/components/Quote.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import QuoteContainer from './QuoteContainer';
+
+function Quote() {
+  return (
+    <div>
+      <nav className="header">
+        <h1 className="title">Math Magicians</h1>
+        <ul className="menu">
+          <li className="menu-item">
+            <Link to="/Math-Magicians">Home</Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/Math-Magicians/calculator">Calculator</Link>
+          </li>
+          <li className="menu-item">
+            <Link to="/Math-Magicians/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+      <QuoteContainer />
+    </div>
+  );
+}
+
+export default Quote;
